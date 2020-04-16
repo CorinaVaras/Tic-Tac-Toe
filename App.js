f8e432cb2543ffc3c556432674b777412ca5f97b
Corina <corinacvp@gmail.com>
2020-04-16 19:18:31 -0400

    fuction onTilePress

--- App.js
+++ App.js
@@ -8,16 +8,16 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
   TouchableOpacity,
+  Alert,
+  ImageBackground,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
 
 
 Icon.loadFont();
@@ -38,13 +38,49 @@ export default class App extends  React.Component {
 
   inicializeGame = () => {
     this.setState({
-      gameState:[[1,-1,0],[0,0,0],[0,0,0]]
+      gameState:[[0,0,0],[0,0,0],[0,0,0]],
+      currentPlayer: 1
 
     })
   }
+  // returns 1 if player 1 won, -1 if player 2 won, or a 0 if no one has won
+  getWinner = () => {
+    let sum;
+    let NUM_TILES= 3;
+    let arr = this.state.gameState;
 
+    // Check rows...
+    for(let i=0; i < NUM_TILES; i++){
+        sum = arr[i][0] + arr[i][1] + arr[i][2];
+        if(sum === 3){ return 1}
+        else if(sum === -3) { return -1};
+    }
+    // Check colums...
+    for(let i=0; i < NUM_TILES; i++){
+      sum = arr[0][i] + arr[1][i] + arr[2][i];
+      if(sum === 3){ return 1}
+      else if(sum === -3) { return -1};
+    }
+    // Check the diagonals...
+    sum = arr[0][0] + arr[1][1] + arr[2][2];
+      if(sum === 3){ return 1}
+      else if(sum === -3) { return -1};
+
+    sum = arr[2][0] + arr[1][1] + arr[0][2];
+      if(sum === 3){ return 1}
+      else if(sum === -3) { return -1};
+
+    // there are no winners...
+    return 0;  
+    
+  }
 
   onTilePress = (row,col) => {
+    // Don't allow tiles to change / no permite que las fichas cambien
+    let value = this.state.gameState[row][col];
+    if(value !== 0) {return;}
+
+    // Grab current player // Obtiene el jugador actual 
     let currentPlayer = this.state.currentPlayer;
 
     // Set the correct tile... 
@@ -60,14 +96,28 @@ export default class App extends  React.Component {
       currentPlayer: nextPlayer
     })
 
+    // Check for winners... 
+    let winner = this.getWinner();
+    if(winner === 1){
+      Alert.alert('El ganador es el jugador 1');
+      this.inicializeGame();
+    } else if (winner === -1){
+      Alert.alert('El ganador es el jugador 2');
+      this.inicializeGame();
+    }
+
+  }
+
+  onNewGamePress = () => {
+    this.inicializeGame();
   }
 
 
   renderIcon = (row,col) => {
     let value= this.state.gameState[row][col];
     switch(value){
-      case 1: return <Icon name='close' style={styles.tileX}/>;
-      case -1: return <Icon name='circle-outline' style={styles.tileO}/>;
+      case 1: return <Icon name='duck' style={styles.tileX}/>;
+      case -1: return <Icon name='fish' style={styles.tileO}/>;
       default: <View/>
     }
 
@@ -77,8 +127,11 @@ export default class App extends  React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-          
+      <ImageBackground
+            source={require("./assets/reactnative.jpg")}
+            style={styles.container}>
+      
+        <View>  
         <View style={{flexDirection: 'row'}}>  
             <TouchableOpacity onPress={() => this.onTilePress(0,0)} style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0,alignItems: 'center',justifyContent: 'center'}]}>
               {this.renderIcon(0,0)}
@@ -114,9 +167,16 @@ export default class App extends  React.Component {
             {this.renderIcon(2,2)}
           </TouchableOpacity>  
         </View>
+        </View> 
 
+        
+        <TouchableOpacity style={styles.button} onPress={this.onNewGamePress}>
+          <Text style={styles.text}> Reiniciar partida</Text>
+        </TouchableOpacity>
 
-      </View>
+      
+      </ImageBackground>
+    
     );
   }
 }
@@ -125,25 +185,41 @@ export default class App extends  React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   tile: {
-    borderWidth: 5,
+    borderWidth: 3,
     width: 100,
     height: 100,
+    borderColor: '#5BC0EB'
 
   },
   tileX: {
-    color: 'red',
+    color: '#F58F29',
     fontSize: 60,
   },
   tileO: {
-    color: 'green',
-    fontSize: 60,
+    color: '#54F2F2',
+    fontSize: 65,
     
-  }
+  },
+  reset:{
+    marginTop: 80,
+  },
+
+  text: {
+    fontSize: 20,
+    textTransform: 'uppercase',
+    color: 'black',
+},
+  button: {
+    marginTop: 65,
+    alignItems: 'center',
+    backgroundColor: '#E8F1F2',
+    padding: 15,
+
+  },
 });
 
 
