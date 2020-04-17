1ae9a1598fbd0c0551b10a262331d783794ef1ef
Corina <corinacvp@gmail.com>
2020-04-17 19:52:41 -0400

    style seccion turno del jugador

--- App.js
+++ App.js
@@ -7,48 +7,36 @@
  */
 
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Alert,
-  ImageBackground,
-} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Alert, ImageBackground} from 'react-native';
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-
-
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 Icon.loadFont();
-export default class App extends  React.Component {
-
-  constructor(props){
+export default class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      gameState:[[0,0,0],[0,0,0],[0,0,0]],
-      currentPlayer: 1
-    }
-
+      gameState: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+      currentPlayer: 1,
+    };
   }
 
   componentDidMount = () => {
     this.inicializeGame();
-  }
+  };
 
   inicializeGame = () => {
     this.setState({
-      gameState:[[0,0,0],[0,0,0],[0,0,0]],
-      currentPlayer: 1
-
-    })
-  }
+      gameState: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+      currentPlayer: 1,
+    });
+  };
 
   checkDraw = () => {
-    const num_tiles = 3
-    let arr = this.state.gameState
+    const num_tiles = 3;
+    let arr = this.state.gameState;
 
-    for (let i=0; i < num_tiles ; i++) {
+    for (let i = 0; i < num_tiles; i++) {
       if (arr[0][i] === 0) {
         return 0;
       } else if (arr[1][i] === 0) {
@@ -58,209 +46,303 @@ export default class App extends  React.Component {
       }
     }
     return 1;
-
-  }
+  };
 
   // returns 1 if player 1 won, -1 if player 2 won, or a 0 if no one has won
   getWinner = () => {
     let sum;
-    let NUM_TILES= 3;
+    let NUM_TILES = 3;
     let arr = this.state.gameState;
 
     // Check rows...
-    for(let i=0; i < NUM_TILES; i++){
-        sum = arr[i][0] + arr[i][1] + arr[i][2];
-        if(sum === 3){ return 1}
-        else if(sum === -3) { return -1};
+    for (let i = 0; i < NUM_TILES; i++) {
+      sum = arr[i][0] + arr[i][1] + arr[i][2];
+      if (sum === 3) {
+        return 1;
+      } else if (sum === -3) {
+        return -1;
+      }
     }
     // Check colums...
-    for(let i=0; i < NUM_TILES; i++){
+    for (let i = 0; i < NUM_TILES; i++) {
       sum = arr[0][i] + arr[1][i] + arr[2][i];
-      if(sum === 3){ return 1}
-      else if(sum === -3) { return -1};
+      if (sum === 3) {
+        return 1;
+      } else if (sum === -3) {
+        return -1;
+      }
     }
     // Check the diagonals...
     sum = arr[0][0] + arr[1][1] + arr[2][2];
-      if(sum === 3){ return 1}
-      else if(sum === -3) { return -1};
+    if (sum === 3) {
+      return 1;
+    } else if (sum === -3) {
+      return -1;
+    }
 
     sum = arr[2][0] + arr[1][1] + arr[0][2];
-      if(sum === 3){ return 1}
-      else if(sum === -3) { return -1};
+    if (sum === 3) {
+      return 1;
+    } else if (sum === -3) {
+      return -1;
+    }
 
     // there are no winners...
-    return 0;  
-    
-  }
+    return 0;
+  };
 
-  onTilePress = (row,col) => {
+  onTilePress = (row, col) => {
     // Don't allow tiles to change / no permite que las fichas cambien
     let value = this.state.gameState[row][col];
-    if(value !== 0) {return;}
+    if (value !== 0) {
+      return;
+    }
 
-    // Grab current player // Obtiene el jugador actual 
+    // Grab current player // Obtiene el jugador actual
     let currentPlayer = this.state.currentPlayer;
 
-    // Set the correct tile... 
+    // Set the correct tile...
     let arr = this.state.gameState.slice();
     arr[row][col] = currentPlayer;
     this.setState({
-      gameState:arr
+      gameState: arr,
     });
 
     // Switch to other player...
-    let nextPlayer = (currentPlayer == 1) ? -1 : 1;
+    let nextPlayer = currentPlayer == 1 ? -1 : 1;
     this.setState({
-      currentPlayer: nextPlayer
-    })
+      currentPlayer: nextPlayer,
+    });
 
-    // Check for winners... 
+    // Check for winners...
     let winner = this.getWinner();
-    if(winner === 1){
+    if (winner === 1) {
       Alert.alert('El ganador es el Duck');
       this.inicializeGame();
-    } else if (winner === -1){
+    } else if (winner === -1) {
       Alert.alert('El ganador es el Fish');
       this.inicializeGame();
     }
     // Check Draw
-    let draw = this.checkDraw()
+    let draw = this.checkDraw();
     if (draw === 1) {
-      Alert.alert('Empate!')
+      Alert.alert('Empate!');
       this.inicializeGame();
     }
-
-  }
+  };
 
   playAgain = () => {
     this.inicializeGame();
-  }
+  };
 
-
-  renderIcon = (row,col) => {
-    let value= this.state.gameState[row][col];
-    switch(value){
-      case 1: return <Icon name='duck' style={styles.duck}/>;
-      case -1: return <Icon name='fish' style={styles.fish}/>;
-      default: <View/>
+  renderIcon = (row, col) => {
+    let value = this.state.gameState[row][col];
+    switch (value) {
+      case 1:
+        return <Icon name="duck" style={styles.duck} />;
+      case -1:
+        return <Icon name="fish" style={styles.fish} />;
+      default:
+        <View />;
     }
-  
-  }
+  };
 
   renderGamerCurrent = () => {
-    let currentPlayer = this.state.currentPlayer
-    switch(currentPlayer) {
-      case 1: return <View style={styles.player} ><Text style={styles.playerDuck}> Jugador 1 </Text><Icon name='duck' style={styles.duckIcon}/></View>;
-      case -1: return <View style={styles.player} ><Text style={styles.playerFish}> Jugador 2 </Text><Icon name='fish'style={styles.fishIcon}/></View>;
+    let currentPlayer = this.state.currentPlayer;
+    switch (currentPlayer) {
+      case 1:
+        return (
+          <View style={styles.player}>
+            <Text style={styles.playerDuck}> Jugador 1 </Text>
+            <Icon name="duck" style={styles.duckIcon} />
+          </View>
+        );
+      case -1:
+        return (
+          <View style={styles.player}>
+            <Text style={styles.playerFish}> Jugador 2 </Text>
+            <Icon name="fish" style={styles.fishIcon} />
+          </View>
+        );
     }
-  }
-
+  };
 
   render() {
     return (
-      
-      <ImageBackground source={require("./assets/reactnative.jpg")} style={styles.container}>
-
+      <ImageBackground
+        source={require('./assets/reactnative.jpg')}
+        style={styles.container}>
         <View style={styles.containerGameCurrent}>
           <Text style={styles.gamerCurrent}>Turno de:</Text>
           {this.renderGamerCurrent()}
         </View>
-        <View style={{ backgroundColor: 'rgba(255,255,255,0.4)'}}>  
-        <View style={{flexDirection: 'row'}}>  
-            <TouchableOpacity onPress={() => this.onTilePress(0,0)} style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0,alignItems: 'center',justifyContent: 'center'}]}>
-              {this.renderIcon(0,0)}
-            </TouchableOpacity>  
-            <TouchableOpacity onPress={() => this.onTilePress(0,1)} style={[styles.tile, { borderTopWidth: 0,alignItems: 'center',justifyContent: 'center'}]}>
-              {this.renderIcon(0,1)}
+        <View style={{backgroundColor: 'rgba(255,255,255,0.4)'}}>
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(0, 0)}
+              style={[
+                styles.tile,
+                {
+                  borderLeftWidth: 0,
+                  borderTopWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(0, 0)}
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => this.onTilePress(0,2)} style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0, alignItems: 'center',justifyContent: 'center'}]}>
-              {this.renderIcon(0,2)}
+            <TouchableOpacity
+              onPress={() => this.onTilePress(0, 1)}
+              style={[
+                styles.tile,
+                {
+                  borderTopWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(0, 1)}
             </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(0, 2)}
+              style={[
+                styles.tile,
+                {
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(0, 2)}
+            </TouchableOpacity>
+          </View>
+
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(1, 0)}
+              style={[
+                styles.tile,
+                {
+                  borderLeftWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(1, 0)}
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(1, 1)}
+              style={[
+                styles.tile,
+                {alignItems: 'center', justifyContent: 'center'},
+              ]}>
+              {this.renderIcon(1, 1)}
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(1, 2)}
+              style={[
+                styles.tile,
+                {
+                  borderRightWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(1, 2)}
+            </TouchableOpacity>
+          </View>
+
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(2, 0)}
+              style={[
+                styles.tile,
+                {
+                  borderLeftWidth: 0,
+                  borderBottomWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(2, 0)}
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(2, 1)}
+              style={[
+                styles.tile,
+                {
+                  borderBottomWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(2, 1)}
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.onTilePress(2, 2)}
+              style={[
+                styles.tile,
+                {
+                  borderRightWidth: 0,
+                  borderBottomWidth: 0,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+              ]}>
+              {this.renderIcon(2, 2)}
+            </TouchableOpacity>
+          </View>
         </View>
 
-        <View style={{flexDirection: 'row'}}>  
-          <TouchableOpacity onPress={() => this.onTilePress(1,0)} style={[styles.tile, { borderLeftWidth: 0, alignItems: 'center',justifyContent: 'center'}]}>
-            {this.renderIcon(1,0)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.onTilePress(1,1)} style={[styles.tile, { alignItems: 'center',justifyContent: 'center' }]}>
-          {this.renderIcon(1,1)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.onTilePress(1,2)} style={[styles.tile, { borderRightWidth: 0, alignItems: 'center',justifyContent: 'center' }]}>
-          {this.renderIcon(1,2)}
-          </TouchableOpacity>
-        </View>
-
-        <View style={{flexDirection: 'row'}}>  
-          <TouchableOpacity onPress={() => this.onTilePress(2,0)} style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0, alignItems: 'center',justifyContent: 'center'}]}>
-             {this.renderIcon(2,0)}
-          </TouchableOpacity>   
-          <TouchableOpacity onPress={() => this.onTilePress(2,1)} style={[styles.tile, { borderBottomWidth: 0, alignItems: 'center',justifyContent: 'center'}]}>
-            {this.renderIcon(2,1)}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.onTilePress(2,2)} style={[styles.tile, { borderRightWidth:0, borderBottomWidth:0, alignItems: 'center',justifyContent: 'center' }]}>
-            {this.renderIcon(2,2)}
-          </TouchableOpacity>  
-        </View>
-        </View> 
-
-        
         <TouchableOpacity style={styles.button} onPress={this.playAgain}>
-          <Text style={styles.text}> Reiniciar partida</Text>
+          <Text style={styles.text}>Volver a jugar</Text>
         </TouchableOpacity>
-
-      
       </ImageBackground>
-      
-        
     );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    
   },
   tile: {
     borderWidth: 3,
     width: 100,
     height: 100,
-    borderColor: '#DEE3E2'
-
+    borderColor: '#DEE3E2',
   },
 
   duck: {
     color: '#DE7119',
     fontSize: 60,
   },
-  
+
   fish: {
     color: '#116979',
     fontSize: 70,
-    
   },
-  
+
   text: {
     fontSize: 20,
     textTransform: 'uppercase',
     color: 'black',
-},
+  },
   button: {
     marginTop: 65,
     alignItems: 'center',
     backgroundColor: '#E8F1F2',
     padding: 15,
-
   },
   containerGameCurrent: {
     display: 'flex',
     flexDirection: 'row',
-    paddingBottom: 62,
+    marginBottom: 85,
     alignItems: 'center',
-},
+  },
   gamerCurrent: {
     fontSize: 30,
     fontWeight: 'bold',
@@ -269,30 +351,29 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     backgroundColor: 'rgba(255,255,255,0.2)',
-    padding: 5, 
-},
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    marginLeft: 5,
+  },
 
-playerDuck: {
-  fontSize: 28,
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center'
-},
-playerFish: {
+  playerDuck: {
     fontSize: 28,
     display: 'flex',
     flexDirection: 'row',
-},
-duckIcon: {
-  color: '#DE7119',
-  fontSize: 40,
-},
+  },
+  playerFish: {
+    fontSize: 28,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  duckIcon: {
+    color: '#DE7119',
+    fontSize: 40,
+  },
 
-fishIcon: {
-  color: '#116979',
-  fontSize: 40,
-  
-},
+  fishIcon: {
+    color: '#116979',
+    fontSize: 45,
+  },
 });
-
-
